585e211bea337c26160efcb3992d78a62a039c4a
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-01-17 13:35:26 -0300

    Rotas do projeto

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,36 @@ const routes = [
     children: [{ path: "", component: () => import("pages/Index.vue") }]
   },
   {
+    path: "/courses",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Courses.vue") }]
+  },
+  {
+    path: "/jobs",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Jobs.vue") }]
+  },
+  {
+    path: "/new-product",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/NewProduct.vue") }]
+  },
+  {
+    path: "/new-job",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/NewJob.vue") }]
+  },
+  {
+    path: "/new-course",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/NewCourse.vue") }]
+  },
+  {
+    path: "/rate-student",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/RateStudent.vue") }]
+  },
+  {
     path: "*",
     component: () => import("pages/Error404.vue")
   }
