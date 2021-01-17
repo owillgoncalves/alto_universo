69382b14eb7d2af203a4984682fb6208c39de484
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-01-17 15:38:19 -0300

    Finalizando página de cursos

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -1,18 +1,22 @@
 <template>
   <q-page class="flex full-width row wrap justify-center items-start content-start">
     <h4 class="text-deep-purple">Cursos disponíveis</h4>
-    <q-card 
-          class="my-card" 
-          v-for="course in courses"
-          :key="course.id">
-      <q-card-section horizontal>
-        <q-img
-          class="col-5 my-image"
-          :src="require('../assets/images/courses/'+ course.id + '.png')"
-          
-        />
+    <q-card class="my-card" flat bordered v-for="course in courses" :key="course.id">
+      <q-card-section horizontal class="my-card-content">
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">{{ course.school }}</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">{{ course.title }}</div>
+          <div class="text-caption text-grey">
+            {{ course.description }}
+          </div>
+        </q-card-section>
 
-        <q-card-section class="my-text">{{ course.description }}</q-card-section>
+        <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            :src="require('../assets/images/courses/' + course.id + '.png')"
+          />
+        </q-card-section>
       </q-card-section>
 
       <q-separator />
@@ -20,14 +24,9 @@
       <q-card-actions>
         <q-btn flat round icon="school" />
         <q-btn flat>
-          {{ course.length }}
-        </q-btn>
-        <q-btn flat>
           {{ course.type }}
         </q-btn>
-        <q-btn flat color="primary">
-          Se inscrever
-        </q-btn>
+        <q-btn flat color="deep-purple"> Inscreva-se </q-btn>
       </q-card-actions>
     </q-card>
   </q-page>
@@ -35,23 +34,56 @@
 
 <script>
 export default {
-  name: 'PageIndex',
-  data () {
+  name: "PageIndex",
+  data() {
     return {
       courses: [
         {
-          id: 'c01',
-          description: `TECNESCOLA
-
-          Eletricidade Básica
-          `,
-          length: '20h',
-          type: 'ONLINE',
+          id: "c01",
+          school: "Tecnescola",
+          title: "Eletricidade Básica",
+          description: `Aprenda os fundamentos da eletricidade com profissionais que têm vasta experiência no mercado`,
+          type: "Online",
         },
-      ]
-    }
-  }
-}
+        {
+          id: "c02",
+          school: "Belezarte",
+          title: "Maquiagem e Penteados",
+          description: `O objetivo deste curso é apresentar aos profissionais da área, técnicas básicas de penteados e maquiagens`,
+          type: "Presencial",
+        },
+        {
+          id: "c03",
+          school: "Belezarte",
+          title: "Cabeleireiro Básico",
+          description: `Curso voltado para os procedimentos de embelezamento e cuidados nos cabelos`,
+          type: "Presencial",
+        },
+        {
+          id: "c04",
+          school: "Escola de TI",
+          title: "Gestão de E-commerce",
+          description: `Este curso tem como objetivo apresentar elementos atualizados sobre como se processa a venda feita via meios eletrônicos`,
+          type: "Online",
+        },
+        {
+          id: "c05",
+          school: "Venda Mais",
+          title: "Representante Comercial",
+          description: `O Representante Comercial é responsável por realizar prospecção de clientes, negociação e vendas externas`,
+          type: "Online",
+        },
+        {
+          id: "c06",
+          school: "Casa Brasil",
+          title: "Cozinha Brasileira",
+          description: `O objetivo deste curso é apresentar técnicas para o preparo de diversos tipos de pratos da culinária brasileira`,
+          type: "Presencial",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="sass" scoped>
@@ -60,9 +92,12 @@ export default {
   max-width: 350px
   margin: 5px
 
+.my-card-content
+  height: 250px
+
 .my-image
   height: 150px
 
-.my-text 
+.my-text
   white-space: pre-line
 </style>
